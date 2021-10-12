8e5cacfd3288883a0be37ebf1136a04a300be990
Gian Federspiel <gian.federspiel@bluewin.ch>
2021-10-12 12:29:25 +0200

    fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const navSlide = () => {
 //Animate links
   navlinks.forEach((link, index) => {
     if(link.style.animation){
-      link.style.animation = ''
+      link.style.animation = '';
     }
     else {
       link.style.animation = `navLinkFade 0.5s ease forward ${index / 5 + 1.5}s`;
