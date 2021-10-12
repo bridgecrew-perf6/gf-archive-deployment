cc80b7748c03ab38f6f384d7ada6a48b1711a94c
Gian Federspiel <gian.federspiel@bluewin.ch>
2021-10-12 11:59:22 +0200

    bugfix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function navSlide() {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-
+  document.getElementById("demo").innerHTML = "Hello World";
+  
   burger.addEventListener('click', function() {
     nav.classList.toggle('nav-active');
-    document.getElementById("demo").innerHTML = "Hello World";
+
   });
 }
 navSlide();
