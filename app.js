6308764482371d2003fe75632166324164480b31
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-03-09 16:18:02 +0100

    Initialized Pages

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
+const path = require ('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('view engine','ejs'); 
+//Send public files
+app.use(express.static(path.join(__dirname, '/src/public')));
+console.log(__dirname);
 
-app.get('/test', (req, res) => {
-    res.render(__dirname + '/src/views/test.ejs');
+//view engine
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {  
+    res.redirect('/home');
 });
 
-app.get('/', (req, res) => {
-    res.render(__dirname + '/src/views/index.ejs');
+app.get('/home', (req, res) => {
+    const header = {currSite: 1};  
+    res.render(__dirname + '/src/views/index.ejs', {header});
 });
+
+app.get('/blog', (req, res) => {    
+    const header = {currSite: 2};  
+    res.render(__dirname + '/src/views/blog.ejs', {header});
+});
+
+app.get('/contact', (req, res) => {
+    const header = {currSite: 3};
+    res.render(__dirname + '/src/views/contact.ejs', {header});
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
   });
