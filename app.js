63eb449c50541b8c82cda5ad0d946ab391770eea
gifederspiel <gian.fedrespiel@bluewin.ch>
2022-03-10 20:06:19 +0100

    Contact Form

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const path = require ('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const SMTPConnection = require('nodemailer/lib/smtp-connection');
+const { getMaxListeners } = require('process');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 //Send public files
 app.use(express.static(path.join(__dirname, '/src/public')));
 console.log(__dirname);
@@ -32,3 +38,46 @@ app.get('/contact', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
   });
+app.post("/contact", async (req, res) =>{
+    var firstname = req.body.formFirstname
+    var lastname = req.body.formLastname
+    var email = req.body.formEmail
+    var message = req.body.formMessage
+    
+    //Bestätigungsmail
+    const transporterCustomer = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: "465",
+        auth: {
+            user: "gf.archivee@gmail.com",
+            pass: "Gian_2004"
+        },
+    });
+    const mailOptionsCustomer = {
+        from: 'gf.archivee@gmail.com',
+        to: email,
+        subject: "You'll hear from me soon",
+        text: "Hallo " + firstname +"\n\nVielen Dank für deine Nachricht. Ich werde deine Anfrage so zeitnah, wie möglich bearbeiten.\n\nLiebe Grüsse\nGian"
+    };
+
+    //Infomail an mich
+    const transporterAdmin = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: "465",
+        auth: {
+            user: "gf.archivee@gmail.com",
+            pass: "Gian_2004"
+        },
+    });
+    const mailOptionsAdmin = {
+        from: 'gf.archivee@gmail.com',
+        to: 'gian.federspiel@bluewin.ch',
+        subject: "Neue Kontaktanfrage",
+        text: "Kontaktanfrage von: " + firstname + " " + lastname + "\nEmail: " + email + "\n\nInhalt: " + message
+    }
+
+    await transporterCustomer.sendMail(mailOptionsCustomer);
+    await transporterAdmin.sendMail(mailOptionsAdmin);
+
+    res.redirect("/contact");
+});
---
+++ src/public/controller.js
@@ -0,0 +1,3 @@
+function mobileDropdown(){
+    window.innerWidth
+}
